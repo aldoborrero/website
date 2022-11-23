768877ff7b48092422859b4e927f6133aeaecbef
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2022-11-23 13:26:15 +0000

    chore: removal of extra non necessary providers

--- .nix/overlays.nix
+++ .nix/overlays.nix
@@ -1,13 +1,5 @@
 super: prev: {
   tf-custom = prev.terraform.withPlugins (p: [
     p.cloudflare
-    p.external
-    p.hcloud
-    p.local
-    p.null
-    p.random
-    p.secret
-    p.time
-    p.tls
   ]);
 }
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,14 +1,4 @@
 terraform {
-  required_providers {
-    cloudflare = { source = "cloudflare/cloudflare" }
-    external   = { source = "hashicorp/external" }
-    null       = { source = "hashicorp/null" }
-    random     = { source = "hashicorp/random" }
-    secret     = { source = "numtide/secret" }
-    time       = { source = "hashicorp/time" }
-    tls        = { source = "hashicorp/tls" }
-  }
-
   backend "s3" {
     bucket                      = "nyx-tf"
     encrypt                     = true
@@ -18,5 +8,9 @@ terraform {
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
+  }
+
+  required_providers {
+    cloudflare = { source = "cloudflare/cloudflare" }
   }
 }
